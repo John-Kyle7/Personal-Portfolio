fa3c1ebe0bf6bf2deca2cddc6e75874797074ddb
John-Kyle7 <148840555+John-Kyle7@users.noreply.github.com>
2023-12-26 09:48:09 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('mobile-menu').addEventListener('click', function () {
+    document.querySelector('.nav-list').classList.toggle('show');
+  });
+  
+
+  lightGallery(document.querySelector('.gallery'));
